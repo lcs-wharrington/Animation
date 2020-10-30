1e24022ae7aaf391308464a018f85ad89fb041e3
William Harrington <WHarrington@lcs.on.ca>
2020-10-30 15:28:06 -0400

    Completely finished poster

--- Playground.playground/Pages/J.Cole desighn.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/J.Cole desighn.xcplaygroundpage/Contents.swift
@@ -16,13 +16,19 @@ PlaygroundPage.current.liveView = canvas
 //Free code start
 canvas.drawShapesWithBorders = false
 let yellow = Color(hue: 58, saturation: 100, brightness: 100, alpha: 100)
-let blue = Color(hue: 224, saturation: 64, brightness: 47, alpha: 50)
+let blue = Color(hue: 224, saturation: 64, brightness: 60, alpha: 50)
 let OffWhite = Color(hue: 81, saturation: 5, brightness: 88, alpha: 100)
 let LimeGreen = Color(hue: 106, saturation: 64, brightness: 69, alpha: 100)
-
+let Grey = Color(hue: 0, saturation: 0, brightness: 75, alpha: 100)
 canvas.fillColor = LimeGreen
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
-canvas.textColor = Color.black
+canvas.textColor = Grey
+canvas.drawText(message: "$136.00/Thursday", at: Point(x: 0, y: 585), size: 10, kerning: 1)
+canvas.drawText(message: "American Airlines Arena", at: Point(x: 125, y: 585), size: 10, kerning: 1)
+canvas.drawText(message: "Miami", at: Point(x: 125, y: 575), size: 10, kerning: 1)
+canvas.drawText(message: "fifth studio album,", at: Point(x: 280, y: 585), size: 10, kerning: 1)
+canvas.drawText(message: "KOD (2018)", at: Point(x: 280, y: 575), size: 10, kerning: 1)
+canvas.drawText(message: "August 9, 2018", at: Point(x: 0, y: 575), size: 10, kerning: 1)
 canvas.drawText(message: "J.Cole", at: Point(x: 0, y: 405), size: 60, kerning: 1)
 for y in stride(from: 0, through: 300, by: 100){
     for x in stride(from: 0, through: 300, by: 100){
