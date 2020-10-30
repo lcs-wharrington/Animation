c3df479f394148448d09935b8a72668174528435
William Harrington <WHarrington@lcs.on.ca>
2020-10-30 09:30:06 -0400

    almost finished the design

--- Playground.playground/Pages/J.Cole desighn.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/J.Cole desighn.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //Required Code
 //Canvas Sizeing
-let preferredWidth = 500
-let preferredHeight = 500
+let preferredWidth = 400
+let preferredHeight = 600
 
 import Cocoa
 import PlaygroundSupport
@@ -14,18 +14,31 @@ let canvas = Canvas(width: preferredWidth, height: preferredHeight)
 PlaygroundPage.current.liveView = canvas
 //Required Code end
 //Free code start
+canvas.drawShapesWithBorders = false
+let yellow = Color(hue: 58, saturation: 100, brightness: 100, alpha: 100)
+let blue = Color(hue: 224, saturation: 64, brightness: 47, alpha: 50)
+let OffWhite = Color(hue: 81, saturation: 5, brightness: 88, alpha: 100)
+let LimeGreen = Color(hue: 106, saturation: 64, brightness: 69, alpha: 100)
 
-let yellow = Color(hue: 333, saturation: 85, brightness: 89, alpha: 100)
-let blue = Color(hue: 224, saturation: 64, brightness: 47, alpha: 100)
+canvas.fillColor = LimeGreen
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
 for y in stride(from: 0, through: 300, by: 100){
     for x in stride(from: 0, through: 300, by: 100){
         
+        canvas.fillColor = OffWhite
+        canvas.drawRectangle(at: Point(x: x + 50, y: y + 50), width: 75, height: 75, anchoredBy: .centre)
+        canvas.fillColor = yellow
+        canvas.drawEllipse(at: Point(x: x + 50, y: y + 50), width: 50, height: 50)
+       
         canvas.fillColor = blue
-        canvas.draw
-       
-        canvas.drawEllipse(at: Point(x: x + 50, y: y + 50), width: 65, height: 65)
-       
+        var vertices1: [Point] = [ ] // empty list of vertices
+        
+        vertices1.append(Point (x: x, y: y + 100))
+        vertices1.append(Point (x: x + 100, y: y))
+        vertices1.append(Point (x: x + 100, y: y + 100))
+        
+        canvas.drawCustomShape(with: vertices1)
        
     }
 }
